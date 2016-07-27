d751d0c1660ddf36930579950ecfa905dd5c3651
iurimenin <iuri020293@>
2016-07-26 21:54:49 -0300

    Começando a brincar

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var nome:String = "Iuri"
+
+print ("olá " + nome + ("."))
+
+var int:Int = 9
+
+int = int * 2
+
+int = int / 4
+
+var outroInt = 7
+
+print(int * outroInt)
+
+print("o valor do int é \(int)")
+
+var numero:Double = 8.3
+
+print(numero * Double(int))
+
+var isMasc:Bool = true
+
+var inteiro:Int = 7
+var double:Double = 4.3
+
+print("O valor da multiplicação de \(int) por \(double) é \(Double(int) * double)")
